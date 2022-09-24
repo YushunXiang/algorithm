bb262a1a6b9225c05a89b2d9aa308a847eaa6263
ysxiang <Yushun.Xiang@foxmail.com>
2022-09-25 01:20:19 +0800

    add solution 28

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,35 @@ import java.util.Scanner;
  * @author ysxiang
  */
 public class Main{
-    private final static int N = 1_000_000_007;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(new Main().getGoodDivisor(n));
+        int k = scanner.nextInt();
+        System.out.print(new Main().getResult(n, k));
     }
 
     /**
-     * solution.
+     * 递归
+     * @param n
+     * @param k
+     * @return
      */
-    public int getGoodDivisor(int n) {
-        
-        if (n <= 3) {
-            return n;
+    public int getResult(int n, int k) {
+        if (n == 1 && k == 1) {
+            return 0;
         }
-        int a = n / 3, b = n % 3;
-        if (b == 1) {
-            return (int) (quickPow(3, a - 1, N) * 4 % N);
-        } else if (b == 2) {
-            return (int) (quickPow(3, a, N) * 2 % N);
+        int middle = 1 << (n - 1); // 2 ^ (n - 1)
+        if (k == middle) {
+            return 1;
+        } else if (k < middle) {
+            return getResult(n - 1, k);
         } else {
-            return (int) quickPow(3, a, N);
+            k = (middle << 1) - k;
+            return invert(getResult(n - 1, k));
         }
     }
-    
 
-    /**
-    * 快速求幂：
-    * p^q，计算中防止溢出，对MOD求余
-    */
-    public long quickPow(int p, int q, int MOD) {
-        long ans = 1L;
-        long base = p;
-        while (q != 0) {
-            if (q % 2 == 1) {
-                ans = ans * base % MOD;
-            }
-            base = base * base % MOD;
-            q = q / 2;
-        }
-        return ans;
+    public int invert(int var) {
+        return 1 - var;
     }
 }
